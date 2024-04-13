637ab288bbb8c5e54875f01394dbeb130cee6042
Muhammed Hanifi Dikmen <dikmen@live.com>
2024-04-13 20:13:33 +0300

    login and logout added with protectedroute comp

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,39 @@
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  //1.Load authedticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  //2. If there is no authenticated user, redirect to /login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  //3. While loading show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  //4.If there is a user , render the app
+  return children;
+}
+
+export default ProtectedRoute;
